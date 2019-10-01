90286c134f0d41dc58e76cd0d9f987011f0f0118
pedroida <pedrohsida@hotmail.com>
2019-10-01 20:01:08 -0300

    profile edit

---
+++ project/resources/lang/pt-BR/flash.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'profile' => [
+        'success' => 'Perfil atualizado com sucesso!',
+        'error' => 'Houve um problema ao atualizar o perfil.',
+    ],
+
+    'password' => [
+        'success' => 'Senha atualizada com sucesso!',
+        'error' => 'Houve um problema ao atualizar a senha.',
+    ],
+
+    'shared' => [
+        'error' => 'Houve um problema ao realizar a ação.',
+    ],
+];
---
+++ project/resources/lang/pt-BR/phrases.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'name' => 'Nome',
+    'email' => 'Email',
+    'logout' => 'Sair',
+    'login' => 'Entrar',
+    'password' => 'Senha',
+    'profile' => 'Meu Perfil',
+    'register' => 'Cadastrar',
+    'edit_profile' => 'Editar perfil',
+    'change_password' => 'Mudar senha',
+    'current_password' => 'Senha atual',
+    'new_password' => 'Nova senha',
+    'user_info' => 'Informações do usuário',
+    'password_confirmation' => 'Confirmação de senha',
+    'new_password_confirmation' => 'Confirmação de nova senha',
+
+    'buttons' => [
+        'register' => 'Criar conta',
+        'save' => 'De barriga cheia',
+        'login' => 'De mansinho lá vou eu',
+        'change_password' => 'Salve Jorge',
+    ],
+];
